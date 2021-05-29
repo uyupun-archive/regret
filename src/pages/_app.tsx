5aee5091dd251fa463a40319f6c2e0e54ea7a52b
Kazuki Tobita <kazukiti201@gmail.com>
2021-05-30 00:18:48 +0900

    Modify app name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>{ process.env.NEXT_PUBLIC_APP_NAME }</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 };
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 const Index = () => {
   return (
-    <div>コントロールパネル</div>
+    <div>
+      <h1>{ process.env.NEXT_PUBLIC_APP_NAME } コントロールパネル</h1>
+    </div>
   )
 };
 
