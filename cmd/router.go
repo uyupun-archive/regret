fad2bbda2013fb4b9755da9ae8c160a14ba90957
Kazuki Tobita <kazukiti201@gmail.com>
2021-05-30 20:03:40 +0900

    Add api

--- cmd/router.go
+++ cmd/router.go
@@ -1,23 +1,19 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/uyupun/regret/handler"
 )
 
-func main() {
+func newRouter() *echo.Echo {
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/", hello)
+	api := e.Group("/api/v0")
+	api.GET("", handler.AddService)
 
-	e.Logger.Fatal(e.Start(":1323"))
-}
-
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "hello, world")
+	return e
 }
