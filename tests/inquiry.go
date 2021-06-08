0c4ce993cd284ea9e00d18d28144c9581fd6063c
Kazuki Tobita <kazukiti201@gmail.com>
2021-06-08 22:39:28 +0900

    Modify tests/inquiry

--- tests/inquiry.go
+++ tests/inquiry.go
@@ -9,29 +9,50 @@ import (
 )
 
 type Inquiry struct {
-	Message    string
+	Title      string
+	Email      string
 	CategoryId int
+	Message    string
 }
 
 func main() {
-	apiEndpoint := "http://localhost:1323/api/v0/inquiry"
 
 	inquiry := Inquiry{
-		Message:    "hogehoge",
+		Title:      "問い合わせテスト",
+		Email:      "uyupun@gmail.com",
 		CategoryId: 1,
+		Message:    "インターネットが壊れました。",
 	}
 
-	reqBody, err := json.Marshal(inquiry)
+	apiEndpoint := "http://localhost:1323/api/v0/inquiry"
+	reqBody := makeRequestBody(inquiry)
+	req := createRequest(apiEndpoint, reqBody)
+
+	res := postInquiry(req)
+	fmt.Println(res)
+}
+
+func makeRequestBody(inquiry Inquiry) *bytes.Reader {
+	reqBodyBytes, err := json.Marshal(inquiry)
 	if err != nil {
 		panic(err)
 	}
-	req, err := http.NewRequest("POST", apiEndpoint, bytes.NewReader(reqBody))
+
+	reqBodyReader := bytes.NewReader(reqBodyBytes)
+	return reqBodyReader
+}
+
+func createRequest(apiEndpoint string, reqBody *bytes.Reader) *http.Request {
+	req, err := http.NewRequest("POST", apiEndpoint, reqBody)
 	if err != nil {
 		panic(err)
 	}
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", "Bearer hogehoge")
+	return req
+}
 
+func postInquiry(req *http.Request) string {
 	client := new(http.Client)
 	res, err := client.Do(req)
 	if err != nil {
@@ -39,10 +60,14 @@ func main() {
 	}
 	defer res.Body.Close()
 
-	resBody, err := ioutil.ReadAll(res.Body)
+	resBody := makeResponseBody(res)
+	return resBody
+}
+
+func makeResponseBody(res *http.Response) string {
+	resBodyBytes, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println(string(resBody))
+	return string(resBodyBytes)
 }
