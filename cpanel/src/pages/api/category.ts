fdea18dbb51534ed20dcc9096e10b552ef1fde60
Kazuki Tobita <kazukiti201@gmail.com>
2021-06-08 13:42:44 +0900

    Modify api

--- cpanel/src/pages/api/category.ts
+++ cpanel/src/pages/api/category.ts
@@ -14,22 +14,27 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
         service_id: req.query.service_id
       }
     })
-    res.status(200).json(_res.data);
+    return res.status(200).json(_res.data);
   }
 
-  if (req.method === 'POST') {
+  else if (req.method === 'POST') {
     const _res = await axios.post(`${apiEndpoint}/category`, req.body);
-    res.status(200).json(_res.data);
+    return res.status(200).json(_res.data);
   }
 
-  if (req.method === 'PATCH') {
+  else if (req.method === 'PATCH') {
     const _res = await axios.patch(`${apiEndpoint}/category`, req.body);
-    res.status(200).json(_res.data);
+    return res.status(200).json(_res.data);
   }
 
-  if (req.method === 'DELETE') {
+  else if (req.method === 'DELETE') {
     const _res = await axios.delete(`${apiEndpoint}/category`, {data: req.body});
-    res.status(200).json(_res.data);
+    return res.status(200).json(_res.data);
+  }
+
+  else {
+    res.setHeader('Allow', ['GET', 'POST', 'PATCH', 'DELETE']);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 };
 
--- cpanel/src/pages/api/service.ts
+++ cpanel/src/pages/api/service.ts
@@ -10,22 +10,27 @@ const apiEndpoint = process.env.API_ENDPOINT;
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   if (req.method === 'GET') {
     const _res = await axios.get(`${apiEndpoint}/service`);
-    res.status(200).json(_res.data);
+    return res.status(200).json(_res.data);
   }
 
-  if (req.method === 'POST') {
+  else if (req.method === 'POST') {
     const _res = await axios.post(`${apiEndpoint}/service`, req.body);
-    res.status(200).json(_res.data);
+    return res.status(200).json(_res.data);
   }
 
-  if (req.method === 'PATCH') {
+  else if (req.method === 'PATCH') {
     const _res = await axios.patch(`${apiEndpoint}/service`, req.body);
-    res.status(200).json(_res.data);
+    return res.status(200).json(_res.data);
   }
 
-  if (req.method === 'DELETE') {
+  else if (req.method === 'DELETE') {
     const _res = await axios.delete(`${apiEndpoint}/service`, {data: req.body});
-    res.status(200).json(_res.data);
+    return res.status(200).json(_res.data);
+  }
+
+  else {
+    res.setHeader('Allow', ['GET', 'POST', 'PATCH', 'DELETE']);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 };
 
