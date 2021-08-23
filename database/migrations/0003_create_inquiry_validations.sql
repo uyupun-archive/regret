b9754372bf6dfe191304658b57a8d2db846ea74e
Kazuki Tobita <kazukiti201@gmail.com>
2021-08-24 00:35:03 +0900

    Add inquiry_validations table

---
+++ database/migrations/0003_create_inquiry_validations.sql
@@ -0,0 +1,15 @@
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS inquiry_validations(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    service_id INT UNSIGNED NOT NULL,
+    is_required_subject BOOLEAN,
+    is_required_email BOOLEAN,
+    is_required_category BOOLEAN,
+    is_required_text BOOLEAN,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY fk_service_id(service_id) REFERENCES services(id) ON DELETE CASCADE
+);
+
+-- +migrate Down
+DROP TABLE IF EXISTS inquiry_validations;
